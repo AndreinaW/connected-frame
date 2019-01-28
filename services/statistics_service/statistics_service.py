84df3e4a383013e528f4119a08b80f95dab16453
dallanoe <enzo.dalla-nora@etu.unice.fr>
2019-01-28 22:46:54 +0100

    added basic stats gathering. Return not working as intended, answer is bugged as hell

--- services/statistics_service/statistics_service.py
+++ services/statistics_service/statistics_service.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 from http.server import BaseHTTPRequestHandler,HTTPServer
 from os import curdir, sep
+import json
 
 PORT_NUMBER = 8081
-filename = 'data'
+
+
 
 #This class will handles any incoming request from
 #the browser 
@@ -11,21 +13,40 @@ class statistics_service_handler(BaseHTTPRequestHandler):
 
 	def _set_response(self):
 		self.send_response(200)
-		self.send_header('Content-type', 'text/html')
+		self.send_header('Content-type', 'application/json')
 		self.end_headers()
 
 	def encode_resp(self, content):
 		return content.format(self.path).encode('utf-8')
+
+	def set_return_json(self, json_result):
+		self.wfile.write(json_result)
+
 
 	def do_POST(self):
 		if self.path=="/compute_stats":
 			content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
 			post_data = self.rfile.read(content_length) # <--- Gets the data itself
 			self._set_response()
-			self.wfile.write(post_data)
-			
-			with open(filename, 'a+') as file:
-				file.write(post_data.decode('utf-8') + '\n')
+
+			json_data = json.loads(post_data)
+
+			return_array = []
+
+			for entry in json_data:
+				print(entry)
+
+				return_json = {}
+				return_json['gender'] = entry['faceAttributes']['gender']
+				return_json['age'] = entry['faceAttributes']['age']
+
+				json_data_result = json.dumps(return_json)
+				print(json_data_result)
+				return_array.append(json_data_result)
+
+			final_res = json.dumps(return_array)
+
+			self.wfile.write(str.encode(final_res))
 
 try:
 	#Create a web server and define the handler to manage the

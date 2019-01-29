51b1931194c54fea59a32c9da2054bdddf5e0bd8
dallanoe <enzo.dalla-nora@etu.unice.fr>
2019-01-29 11:22:51 +0100

    linked app to other services

--- services/app/app.py
+++ services/app/app.py
@@ -54,19 +54,18 @@ class myHandler(BaseHTTPRequestHandler):
 			conn = http.client.HTTPSConnection(face_api_url)
 			conn.request("POST", face_api_url_extension % params, "{'url': 'https://www.webcreatorbox.com/wp-content/uploads/2013/08/zombie-orizinal.jpg'}", face_api_headers)
 			response = conn.getresponse()
-			data = response.read()
+			data = response.read().decode()
 			print(data)
 			conn.close()
+
+			basic_stats = self.send_basic_post_request(url_stats, data)
+			self.send_basic_post_request(url_dashboard, basic_stats)
 
 			self.send_response(200)
 			self.send_header('Content-type', 'application/json')
 			self.end_headers()
-			self.wfile.write(data)
+			self.wfile.write('{"response":"ok"}')
 			return
-
-			#post_fields = '[{"faceId": "7849bc2e-31a9-45de-8739-7c891da61596","faceRectangle": {"top": 1120,"left": 3226,"width": 1688,"height": 1688},"faceAttributes": {"smile": 0.001,"headPose": {"pitch": 0.0,"roll": 9.6,"yaw": 0.3},"gender": "female","age": 27.0,"emotion": {"anger": 0.0,"contempt": 0.0,"disgust": 0.0,"fear": 0.0,"happiness": 0.001,"neutral": 0.993,"sadness": 0.001,"surprise": 0.004},"exposure": {"exposureLevel": "overExposure","value": 0.82}}}]'
-			#basic_stats = self.send_basic_post_request(url_stats, post_fields)
-			#self.send_basic_post_request(url_dashboard, basic_stats)
 
 			#content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
 			#post_data = self.rfile.read(content_length) # <--- Gets the data itself
